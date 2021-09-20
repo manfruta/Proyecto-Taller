0e303c9002d49086c8f390b377e40b68e8aa1e17
Felipe Espinoza <53890265+fandres21@users.noreply.github.com>
2021-09-20 16:18:42 -0300

    Update

--- taller-flores.sql
+++ taller-flores.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-02-2021 a las 19:33:16
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.4.10
+-- Tiempo de generación: 20-09-2021 a las 21:17:47
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 7.3.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,29 +18,29 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `prueba_login`
+-- Base de datos: `taller-flores`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `typeUser`
+-- Estructura de tabla para la tabla `typeuser`
 --
 
-CREATE TABLE `typeUser` (
+CREATE TABLE `typeuser` (
   `idUserType` int(128) NOT NULL,
   `RoleUser` varchar(128) NOT NULL,
   `descriptionUserRole` varchar(256) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `typeUser`
+-- Volcado de datos para la tabla `typeuser`
 --
 
-INSERT INTO `typeUser` (`idUserType`, `RoleUser`, `descriptionUserRole`) VALUES
-(1, 'Administrador', 'Jefe del departamento'),
+INSERT INTO `typeuser` (`idUserType`, `RoleUser`, `descriptionUserRole`) VALUES
+(1, 'Administrador', 'Jefe de Operaciones y Repuestos'),
 (2, 'Secretaria', 'Gestion de Facturas y Boletas'),
-(3, 'Mecánico', 'Encaregado de generar los Presupuestos');
+(3, 'Mecánico', 'Encargado de generar los Presupuestos');
 
 -- --------------------------------------------------------
 
@@ -50,10 +50,10 @@ INSERT INTO `typeUser` (`idUserType`, `RoleUser`, `descriptionUserRole`) VALUES
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL COMMENT 'Identificador único para el usuario',
-  `usuario` varchar(30) NOT NULL COMMENT 'correo electrónico',
-  `clave` varchar(130) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `apellidos` varchar(128) NOT NULL,
+  `User` varchar(30) NOT NULL COMMENT 'correo electrónico',
+  `password` varchar(130) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `lastName` varchar(128) NOT NULL,
   `idUserType` int(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -61,17 +61,18 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `usuario`, `clave`, `nombre`, `apellidos`, `idUserType`) VALUES
-(42, 'jime@correo.de', '$2y$10$FRcsor4X9UHLiUjQ3BqSZe0vnWF/lSH79tLM1kqHSfLcHr8CfY27i', 'Jimena', 'Mora Torres', 1);
+INSERT INTO `usuarios` (`id`, `User`, `password`, `name`, `lastName`, `idUserType`) VALUES
+(1, 'fespinoza21', '$2y$10$FRcsor4X9UHLiUjQ3BqSZe0vnWF/lSH79tLM1kqHSfLcHr8CfY27i', 'Felipe', 'Espinoza', 1),
+(115, 'marze', '$2y$10$FRcsor4X9UHLiUjQ3BqSZe0vnWF/lSH79tLM1kqHSfLcHr8CfY27i', 'Marcelo', 'Luengo', 1);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `typeUser`
+-- Indices de la tabla `typeuser`
 --
-ALTER TABLE `typeUser`
+ALTER TABLE `typeuser`
   ADD PRIMARY KEY (`idUserType`);
 
 --
@@ -85,16 +86,16 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT de la tabla `typeUser`
+-- AUTO_INCREMENT de la tabla `typeuser`
 --
-ALTER TABLE `typeUser`
+ALTER TABLE `typeuser`
   MODIFY `idUserType` int(128) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador único para el usuario', AUTO_INCREMENT=115;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador único para el usuario', AUTO_INCREMENT=116;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
